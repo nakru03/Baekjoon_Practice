8a8c45a33e53ad211c0343a81bcedb06931e24ec
SangHyung Kim <nakru03@gmail.com>
2018-10-01 23:38:47 +0900

    Solve 1978
    about PrimeNumber

---
+++ Beakjoon 단계별 풀이/1978.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	int count = 0;
+	
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		int n;
+		cin >> n;
+		bool flag = true;
+		if (n == 2)
+			flag = true;
+		else if (n == 1)
+			flag = false;
+		else {
+			for (int i = 2; i < n; ++i)
+			{
+				if (n%i == 0)
+					flag = false;
+									
+			}
+		}
+		if (flag == true)
+			count++;
+		
+	}
+	cout << count << endl;
+	return 0;
+}
