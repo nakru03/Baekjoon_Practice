104fa190f5383a9383d9d3e5e53a50933ead6232
SangHyung Kim <nakru03@gmail.com>
2018-10-01 23:37:45 +0900

    Solve 1929
    about  primenumber

---
+++ Beakjoon 단계별 풀이/1929.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+#define MaxValue 1000001
+using namespace std;
+bool arr[MaxValue] = { true, true };
+
+int main()
+{
+	cout.sync_with_stdio(false);
+	int M, N;
+	cin >> M >> N;
+	for (int i = 2; i*i <= N; ++i)
+	{
+		if (!arr[i])
+		{
+			for (int j = i*i; j <= N; j=j+i)
+			{
+				arr[j] = true;
+			}
+		}
+	}
+
+	for (int i = M; i <= N; ++i)
+	{
+		if (!arr[i])
+			cout << i << '\n';
+	}
+	return 0;
+}
