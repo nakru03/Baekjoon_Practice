577444184da6bf55a8cd32f61265b11b1d5b8173
SangHyung Kim <nakru03@gmail.com>
2018-10-01 23:38:16 +0900

    Solve 2581
    about PrimeNumber

---
+++ Beakjoon 단계별 풀이/2581.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	vector<int> arr;	
+	int M, N;
+	int sum = 0;	
+	cin >> M >> N;
+	
+	for (int i = M; i < N+1; ++i)
+	{
+		bool flag=true;
+		if (i <= 1 || N <= 1) //2일때  생각해야한다.
+		{
+			flag = false;
+		}
+		for (int j = 2; j < i; ++j)
+		{
+			if (i%j == 0)
+			{
+				flag = false;
+				break; //break를 통한 연산속도 증가.
+			}				
+			else
+				flag = true;
+		}
+		if (flag)
+		{
+			arr.push_back(i);
+			sum += i;
+		}
+			
+	}
+	if (!arr.size())
+		cout << "-1"<<endl;
+	else
+	{
+		int minimum = *min_element(arr.begin(), arr.end());
+		cout << sum << endl;
+		cout << minimum << endl;		
+	}
+	
+	return 0;
+}
