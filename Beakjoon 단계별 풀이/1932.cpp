bd20477349b572568a0a05e113cd33f0ea3046d6
SangHyung Kim <nakru03@gmail.com>
2018-11-19 20:24:13 +0900

    Solve 1932
    about Dp

--- Beakjoon 단계별 풀이/1932.cpp
+++ Beakjoon 단계별 풀이/1932.cpp
@@ -1,13 +1,39 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int tc;
-//	cin >> tc;
-//	for (int i = 0; i < tc; ++i)
-//	{
-//
-//	}
-//}
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int dp[500][500];
+int memo[500][500];
+int main()
+{
+	int tc;
+	int sum = 0;
+	cin >> tc;
+	for (int i = 0; i < tc; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+			int n;
+			cin >> n;
+			dp[i][j] = n;
+			
+		}
+	}
+	memo[0][0] = dp[0][0];
+	for (int i = 1; i < tc; ++i)
+	{
+		for (int j = 0; j <= i; ++j)
+		{
+
+			memo[i][j] = max(memo[i - 1][j] + dp[i][j], memo[i - 1][j - 1] + dp[i][j]);
+			sum = memo[i][j];
+			
+		}
+		
+	}
+	for (int i = 0; i < tc; ++i)
+	{
+		sum = max(memo[tc - 1][i], sum);
+	}
+	cout << sum << endl;
+}
