1b156c8a00fb48a37bcef3748c57d0339da2fb4d
SangHyung Kim <nakru03@gmail.com>
2018-11-26 17:38:53 +0900

    Solve 1463
    about DP

---
+++ Beakjoon 단계별 풀이/1463.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1000001]={ 0,0 };
+
+int main()
+{
+	int n;
+	//dp[n]은 n까지 시행횟수 저장
+	//bottom up 방식을 이용
+	
+
+	cin >> n;
+	for (int i = 2; i <= n; ++i)
+	{
+		dp[i] = dp[i - 1] + 1; 
+
+		if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+	}
+	cout << dp[n]<<endl;
+	return 0;
+
+}
