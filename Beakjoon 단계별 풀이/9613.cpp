e96e111dedfb1116834f3be17552f9f79c6c8e63
SangHyung Kim <nakru03@gmail.com>
2019-02-20 21:44:05 +0900

    Solve 9613.cpp
    about GCD sum

---
+++ Beakjoon 단계별 풀이/9613.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+int arr[1000001];
+int gcd(int large, int small)
+{
+	if (large % small == 0)
+		return small;
+	else
+		return gcd(small, large%small);
+}
+
+int main()
+{
+	int tc;
+	
+	cin >> tc;
+	
+	for (int i = 0; i < tc; ++i)
+	{
+		int n;
+		long long res = 0; // int로 쓰면 최대일때 int를 초과한다.
+		cin >> n;
+
+		for (int j = 0; j < n; ++j)		
+			cin >> arr[j];
+		
+		
+		for (int j = 0; j < n-1; ++j)
+		{
+			for (int k = j + 1; k < n; ++k)
+				res += gcd(arr[j], arr[k]);
+		}
+		cout << res << endl;
+	}
+
+	
+}
