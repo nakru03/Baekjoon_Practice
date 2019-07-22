d1db3b223ad81b323be2237c559e88278d4c5401
SangHyungKim <nakru03@gmail.com>
2019-07-22 13:28:17 +0900

    [Solve] 7569
    about BFS

--- Beakjoon 문제집풀이/0x05/7569.java
+++ Beakjoon 문제집풀이/0x05/7569.java
@@ -4,13 +4,15 @@ public class BJO7569 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] dx= {0,0,1,-1,0,0};
-		int[] dy= {0,1,0,0,-1,0};
-		int[] dz= {1,0,0,0,0,-1};
+		int[] dx= {1,-1,0,0,0,0};
+		int[] dy= {0,0,1,-1,0,0};
+		int[] dz= {0,0,0,0,1,-1};
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		int h = sc.nextInt();
 		int[][][] boxes = new int[h][n][m];
+		int[][][] day = new int[h][n][m];
+
 		Queue<Triple> q = new LinkedList<Triple>();
 		
 		for(int z=0; z<h; ++z) {
@@ -30,10 +32,39 @@ public class BJO7569 {
 				int nx = curr.third+dx[dir];
 				if(nz<0 || ny <0 || nx < 0 || nz>=h || ny >= n || nx>= m) continue;
 				if(boxes[nz][ny][nx]!=0) continue;
-				
+				boxes[nz][ny][nx] = 1;
+				day[nz][ny][nx] = day[curr.first][curr.second][curr.third] +1;
+				q.offer(new Triple(nz,ny,nx));
 			}
 		}
+
+//		for(int z=0; z<h; ++z) {
+//			for(int y=0; y<n; ++y) {
+//				for(int x=0; x<m; ++x) {
+//					System.out.println(day[z][y][x]);					
+//						
+//					
+//				}
+//			}
+//		}
+		int result = Integer.MIN_VALUE;
+		for(int z=0; z<h; ++z) {
+			for(int y=0; y<n; ++y) {
+				for(int x=0; x<m; ++x) {
+					if(boxes[z][y][x]==0){
+						System.out.println(-1);
+						return;
+					}
+					if(result<day[z][y][x])
+						result=day[z][y][x];
+						
+					
+				}
+			}
+		}
+		System.out.println(result);
 	}
+	
 
 }
 class Triple {
