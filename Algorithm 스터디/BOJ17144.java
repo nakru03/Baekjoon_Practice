cbe9fccf7e9e16688ff4b6afffedb8474d0b2085
SangHyungKim <nakru03@gmail.com>
2019-08-20 14:39:00 +0900

    [Solve] 17144.java

--- Algorithm 스터디/BOJ17144.java
+++ Algorithm 스터디/BOJ17144.java
@@ -20,105 +20,112 @@ public class BOJ17144 {
 		X = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
 		
-		board = new int[Y][X];
-		board2 = new int[Y][X];
+		board = new int[Y][X];		
 		for(int y=0; y<Y; ++y) {
 			st = new StringTokenizer(br.readLine());
 			for(int x=0; x<X; ++x) {
 				board[y][x] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		//출력
-//		for(int y=0; y<Y; ++y) {
-//			for(int x=0; x<X; ++x) {
-//				System.out.print(board[y][x] +" ");
-//			}
-//			System.out.println();
-//			
-//		}
-//		System.out.println();
-		diffusion();
-		System.out.println();
 
-		System.out.println();
-		
-		airClean();
+		for(int i=0; i<T; ++i) {
+			diffusion();	
+			airClean();	
+		}
+		int result = 0;
 		for(int y=0; y<Y; ++y) {
 			for(int x=0; x<X; ++x) {
-				System.out.print(board2[y][x] +" ");
+				if(board[y][x]==-1) continue;
+				result += board[y][x];
 			}
-			System.out.println();			
+			
 		}
+		System.out.println(result);
 	}
 	private static void airClean() {
 		Pair start=null;
 		Pair start2=null;
+		board2 = new int[Y][X];
 		//공청 시작점 찾기
 		out:for(int y=0; y<Y; ++y) {
 			for(int x=0; x<X; ++x) {
 				if(board[y][x] == -1) {
 					start = new Pair(y,x);
-					start2 = new Pair(y-1,x);
+					start2 = new Pair(y+1,x);
 					break out;
 				}
 			}
 		}
-		
-		int tmp = 0;
-		//------->
-		for(int x = start.x+1; x<X; ++x) {
-			int curr = board[start.y][x];
-			int before = board[start.y][x-1];
-			if(before==-1) {
-				curr = 0;
-			}else if(x==x-1) {
-				tmp = curr;
-				curr = before;
-			}
-			else {
-				curr = before;
-			}
+		//위쪽 공기청정기의 start 순환
+		////----->
+		for(int x=start.x+1; x<X; ++x) {
+			board2[start.y][x] = board[start.y][x-1];
+			if(board2[start.y][x]==-1)
+				board2[start.y][x]=0;			
 		}
-		//위로 
-		for(int y = start.y+1; y>=0; --y) {
-			int curr = board[y][start.x];
-			int before = board[y+1][start.x];
-			if(y==start.y+1) {				
-				curr = tmp;
-			}else if(y== 0) {
-				tmp = curr;
-				curr = before;
-			}else {
-				curr = before;
-			}						
+		//위로
+		for(int y=start.y-1; y>=0; --y) {
+			board2[y][X-1] = board[y+1][X-1];
 		}
-		for(int x = X-1; x>=0; --x) {
-			int curr = board[0][x];
-			int before = board[0][x-1];
-			if(x==X-1) {				
-				curr = tmp;
-			}else if(x==0) {
-				tmp = curr;
-				curr = before;
-			}else {
-				curr = before;
-			}						
+		//<--------
+		for(int x=X-2; x>=0; --x) {
+			board2[0][x] = board[0][x+1];
+		}
+		//아래로
+		for(int y=1; y<start.y; ++y) {
+			board2[y][0] = board[y-1][0];
+		}
+		//복사
+		for(int x=start.x+1; x<X; ++x) {
+			board[start.y][x] = board2[start.y][x];
+		}
+		for(int y=start.y-1; y>=0; --y) {
+			board[y][X-1] = board2[y][X-1];
+		}
+		for(int x=X-2; x>=0; --x) {
+			board[0][x] = board2[0][x];
+		}
+		for(int y=1; y<start.y; ++y) {
+			board[y][0] = board2[y][0];
+		}
+		//아래쪽 공기청정기..
+		for(int x=start2.x+1; x<X; ++x) {
+			board2[start2.y][x] = board[start2.y][x-1];
+			if(board2[start2.y][x]==-1)
+				board2[start2.y][x]=0;			
 		}
 		
-		for(int y=0; y<start.y; y++) {
-			int curr = board[y][0];
-			int before = board[y-1][0];
-			if(y==0) {
-				curr = tmp;
-			}else
-				curr = before;
+		//아래로
+		for(int y=start2.y+1; y<Y; ++y) {
+			board2[y][X-1] = board[y-1][X-1];
 		}
-	
-		
+		//<--------
+		for(int x=X-2; x>=0; --x) {
+			board2[Y-1][x] = board[Y-1][x+1];
+		}		
+		//위로
+		for(int y=Y-2; y>start2.y; --y) {
+			board2[y][0] = board[y+1][0];
+		}		
+		//복사
+		for(int x=start2.x+1; x<X; ++x) {
+			board[start2.y][x] = board2[start2.y][x];
+		}
+		for(int y=start2.y+1; y<Y; ++y) {
+			board[y][X-1] = board2[y][X-1];
+		}
+		for(int x=X-2; x>=0; --x) {
+			board[Y-1][x] = board2[Y-1][x];
+		}
+		for(int y=Y-2; y>start2.y; --y) {
+			board[y][0] = board2[y][0];
+		}
 	}
+	
+	
 	private static void diffusion() {
 		Queue<Pair> q = new LinkedList<>();
+		board2 = new int[Y][X];
 		for(int y=0; y<Y; ++y) {
 			for(int x=0; x<X; ++x) {
 				if(board[y][x]>0) {
