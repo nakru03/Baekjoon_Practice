5d352a8b9bc3ebf4772aeb620e931ee84bdad557
SangHyung Kim <nakru03@gmail.com>
2019-09-02 07:26:21 +0900

    [Backup]

--- 백업/BOJ17136.java
+++ 백업/BOJ17136.java
@@ -1,9 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BOJ17136 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int[][] board = new int[10][10];
+	static int[] paper = new int[5];
+	static int answer;
+	static ArrayList<Pair> list;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Arrays.fill(paper, 5);
+		
+		for(int y=0; y<10; ++y) {
+			String[] s = br.readLine().split(" ");
+			for(int x=0; x<10; ++x) {
+				board[y][x] = Integer.parseInt(s[x]);
+				if(board[y][x]==1) {
+					list.add(new Pair(y,x));
+				}
+			}
+		}
+		
+		dfs(0,0);
+		
+		
 	}
-
+	static int cnt;
+	static int min = Integer.MAX_VALUE;
+	
+	private static void dfs(int idx, int depth) {
+		if(depth == list.size()) {
+			min = Math.min(min, cnt);
+			return;
+		}
+		for(int)
+		
+	}
+	static class Pair{
+		int y;
+		int x;
+		public Pair(int y, int x) {
+			super();
+			this.y = y;
+			this.x = x;
+		}
+		
+	}
 }
