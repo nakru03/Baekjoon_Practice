5cc8d84caa9bc5c38482918d0c93c397e63f9e79
SangHyungKim <nakru03@gmail.com>
2019-11-11 15:29:34 +0900

    [Solve] 16197

---
+++ Algorithm 스터디/BOJ16197.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ16197 {
+	static int N;
+	static int M;
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Integer.parseInt(br.readLine());
+	}
+
+}
